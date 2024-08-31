4a9bb38833673216dc2c23f5b9cb3c323867c93d
adnan <adnan.mahmud.pc@gmail.com>
2024-09-01 00:17:58 +0600

    6 bus fare calculation is done

--- JS-conditionals-tasks/6 bus fare calculator.js
+++ JS-conditionals-tasks/6 bus fare calculator.js
@@ -6,3 +6,19 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+
+var age = 70
+
+if(age < 10){
+    console.log('free')
+}
+else if(age >= 10 && age < 60){
+    console.log('50% discount')
+}
+else if(age >= 60){
+    console.log('15% Discount')
+}
+else{
+    console.log('Regular ticket fare 800 tk')
+}
