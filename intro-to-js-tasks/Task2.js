14492ebef2dc9a0da0b7671e1aacd911e60c3cb6
adnan <adnan.mahmud.pc@gmail.com>
2024-08-31 20:16:47 +0600

    Task 2 problem solved

---
+++ intro-to-js-tasks/Task2.js
@@ -0,0 +1,7 @@
+var Mathematics = 75.25;
+var Biology = 65;
+var Chemistry = 80;
+var Physics = 35.45;
+var Bangla = 99.50;
+var avg = (Mathematics + Biology +Physics + Bangla) / 5;
+console.log(avg.toFixed(2));
