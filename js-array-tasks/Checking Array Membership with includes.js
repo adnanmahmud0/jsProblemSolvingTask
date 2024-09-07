892f67a0c93d4597b7748da2e2be202452702a31
adnan <adnan.mahmud.pc@gmail.com>
2024-09-08 00:48:33 +0600

    array task 3 are done

---
+++ js-array-tasks/Checking Array Membership with includes.js
@@ -0,0 +1,7 @@
+let books = ["Python for Beginners", "Learning JavaScript", "Advanced Java", "HTML & CSS", "React for Developers"];
+
+if (books.includes("Learning JavaScript")) {
+    console.log("The JavaScript book is present in the array.");
+} else {
+    console.log("The JavaScript book is not present in the array.");
+}
