48215bdc7ec758984033d1bf93603cb5a37da0c9
adnan <adnan.mahmud.pc@gmail.com>
2024-09-08 01:18:16 +0600

    while task 2 complite

---
+++ WHILE/2 odd even.js
@@ -0,0 +1,31 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+i=61
+while(i<=100){
+    if(i%2!=0){
+        console.log(i)
+    }
+    i++;
+}
+
+i=78
+while(i<=98){
+    if(i%2==0){
+        console.log(i)
+    }
+    i++;
+}
