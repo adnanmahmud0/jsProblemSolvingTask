bc285a792ae12ef55eaf9239f966edc9818af7c1
adnan <adnan.mahmud.pc@gmail.com>
2024-09-08 00:44:44 +0600

    create array task folder and array task 1 and 2 are done

---
+++ js-array-tasks/Add or remove elements.js
@@ -0,0 +1,15 @@
+// 2. Add or remove elements
+// Declare an array of 3 tourist destinations
+// Add a new tourist destination to your tourist array
+// Add two more to your array
+// Remove the last tourist destination you have added
+// display the final array as output
+
+tourist_destinations=['shylet', 'coxes bazar', 'rangpur'];
+console.log(tourist_destinations);
+tourist_destinations.push('Khulna');
+console.log(tourist_destinations);
+tourist_destinations.push('gajipur', 'molobibazar');
+console.log(tourist_destinations);
+tourist_destinations.pop();
+console.log(tourist_destinations);
---
+++ js-array-tasks/Declare an array.js
@@ -0,0 +1,11 @@
+// 1. Declare an array
+// Declare an array with 5 elements containing fruits
+// console log the 3rd index element
+// change the value of the 2nd index element to jambura
+// console log the final array
+
+let fruits = ['apple', 'dragon food', 'litchi', 'guava', 'pineapple'];
+
+console.log(fruits[2]);
+fruits[1]="jambura"
+console.log(fruits)
